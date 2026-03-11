ef8853c7d84e4319e29f2997f047b9da3ceca4db
Leezans <Leezans@outlook.com>
2026-03-11 21:03:33 +0800

    ..

--- src/quantlab/orchestration/__init__.py
+++ src/quantlab/orchestration/__init__.py
@@ -1,7 +1,7 @@
-from quantlab.orchestration.formula_pipeline import FormulaicFactorWorkflow, FormulaicResearchResult
+
 from quantlab.orchestration.factor_pipeline import FactorResearchResult, FactorResearchWorkflow
 from quantlab.orchestration.regime_pipeline import RegimeResearchResult, RegimeResearchWorkflow
-from quantlab.orchestration.pipeline import ResearchRunResult, ResearchWorkflow
+
 
 __all__ = [
     "FactorResearchResult",
